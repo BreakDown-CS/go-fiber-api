e0568b877cf11ebcccce3d925f1fbe1bfd79f0e6
BreakDown-CS <chanon.sungchoom@gmail.com>
2025-12-17 13:01:58 +0700

    add router layer

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/BreakDown-CS/go-fiber-api/router"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
-	})
+	router.Register(app)
 
 	app.Listen(":3000")
 }
